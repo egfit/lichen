a403279698b93f0e6e3b6c0cfaff71b291565cdf
jcai <jcai@fepss.com>
2011-08-20 21:22:36 +0800

    增加了数组性质的参数

--- lichen-orm/src/main/java/com/egfit/lichen/orm/model/QueryRelation.java
+++ lichen-orm/src/main/java/com/egfit/lichen/orm/model/QueryRelation.java
@@ -1,5 +1,6 @@
 package com.egfit.lichen.orm.model;
 
+
 import org.apache.tapestry5.func.F;
 import org.apache.tapestry5.func.Flow;
 import org.apache.tapestry5.func.Reducer;
@@ -10,10 +11,15 @@ import org.apache.tapestry5.func.Reducer;
  */
 public class QueryRelation {
 	private Flow<QlFrame> whereQl;
+	private Flow<Object> parameters;
 	private final Reducer<StringBuilder,QlFrame> whereReducer = new WhereQlReducer();
 
 	public QueryRelation(){
 		this.whereQl= F.flow();
+		parameters = F.flow();
+	}
+	Flow<Object> parameters(){
+		return parameters;
 	}
 	/**
 	 * 给定一个ql片段，加入到条件语句.
@@ -26,8 +32,11 @@ public class QueryRelation {
 	 * @param ql 查询语句片段
 	 * @return 查询对象
 	 */
-	public QueryRelation where(String ql){
-		whereQl=whereQl.append(new WhereQlFrame(ql));
+	public QueryRelation where(String ... conditions){
+		if(conditions.length==0){
+			return this;
+		}
+		whereQl=whereQl.append(new WhereQlFrame(conditions));
 		return this;
 	}
 	void produceWhereCondition(StringBuilder sb){
@@ -38,19 +47,26 @@ public class QueryRelation {
 	}
 	interface QlFrame{
 		String toSql();
+		Flow<?> parameters();
 	}
 	class WhereQlReducer implements Reducer<StringBuilder,QlFrame>{
 		public StringBuilder reduce(StringBuilder accumulator, QlFrame value) {
+			parameters = parameters.concat(value.parameters());
 			return accumulator.append(" and ").append(value.toSql());
 		}
 	}
 	class WhereQlFrame implements QlFrame{
-		private String sql;
-		WhereQlFrame(String sql){
-			this.sql = sql;
+		private String ql;
+		private Flow<String> sqlParameters;
+		WhereQlFrame(String ... conditions){
+			ql = conditions[0];
+			sqlParameters=F.flow(conditions);
 		}
 		public String toSql(){
-			return sql;
+			return ql;
+		}
+		public Flow<?> parameters(){
+			return sqlParameters.drop(1);
 		}
 	}
 }
--- lichen-orm/src/test/java/com/egfit/lichen/orm/model/QueryRelationTest.java
+++ lichen-orm/src/test/java/com/egfit/lichen/orm/model/QueryRelationTest.java
@@ -15,6 +15,14 @@ public class QueryRelationTest {
 		qr.where("name='jcai'");
 		qr.produceWhereCondition(sb);
 		assertEquals("where 1=1 and name='jcai'", sb.toString());
+		assertEquals(0,qr.parameters().count());
+		
+		sb.setLength(0);
+		qr.where("password=?","mypassword");
+		qr.produceWhereCondition(sb);
+		assertEquals("where 1=1 and name='jcai' and password=?", sb.toString());
+		assertEquals(1,qr.parameters().count());
+		assertEquals("mypassword",qr.parameters().first());
 	}
 	private QueryRelation createQueryRelation(){
 		return new QueryRelation();
